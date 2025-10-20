d4cd240127d8862c685e32fd9d4efd833676ddc8
syarifulanam <em.syariful.anam@gmail.com>
2025-10-20 15:53:27 +0700

    merubah tampilan add dan edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <title>App Ternak Terack</title>
+
+    <link rel="icon" 
+          href='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <text y="70" font-size="80">🐄</text></svg>' 
+          type="image/svg+xml">
+
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 
 <body class="bg-light">
-    <div class="container py-5">
+    <div class="container py-5 text-center">
+        {{-- <h1 class="mb-4">🐄 App Ternak Terack</h1> --}}
         @yield('content')
     </div>
+
     <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
