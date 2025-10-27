347c8dc990a7ce3f3d96155b8a8a5a97275ff376
syarifulanam <em.syariful.anam@gmail.com>
2025-10-27 11:52:32 +0700

    sidebar

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -1,22 +1,21 @@
         <!-- Sidebar -->
         <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">Ternak track</div>
             </a>
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+            <li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('dashboard.index') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Dashboard</span>
+                </a>
             </li>
 
             <!-- Divider -->
@@ -24,42 +23,57 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Daily
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+            <li class="nav-item {{ request()->routeIs('growth.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-chart-line"></i>
+                    <span>Growth Records</span>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
             </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+            <li class="nav-item {{ request()->routeIs('health.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-heartbeat"></i>
+                    <span>Health Records</span>
                 </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
-                </div>
+            </li>
+
+            <li class="nav-item {{ request()->routeIs('vaccinations.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-syringe"></i>
+                    <span>Vaccinations</span>
+                </a>
+            </li>
+
+            <li class="nav-item {{ request()->routeIs('feeding.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-utensils"></i>
+                    <span>Feeding Records</span>
+                </a>
+            </li>
+
+            <li class="nav-item {{ request()->routeIs('breeding.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-venus-mars"></i>
+                    <span>Breeding</span>
+                </a>
+            </li>
+
+            <li class="nav-item {{ request()->routeIs('offsprings.*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('offsprings.index') }}">
+                    <i class="fas fa-baby"></i>
+                    <span>Offsprings</span>
+                </a>
+            </li>
+
+            <li class="nav-item {{ request()->routeIs('sales.*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('sales.index') }}">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span>Sales</span>
+                </a>
             </li>
 
             <!-- Divider -->
@@ -67,42 +81,29 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Addons
+                Master Data
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+            <li class="nav-item {{ request()->routeIs('farms.*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('web.farms.index') }}">
+                    <i class="fas fa-tractor"></i>
+                    <span>Farms</span>
                 </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
-                    </div>
-                </div>
             </li>
 
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+            <li class="nav-item {{ request()->routeIs('cages.*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('web.cages.index') }}">
+                    <i class="fas fa-th-large"></i>
+                    <span>Cages</span>
+                </a>
             </li>
 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+            <li class="nav-item {{ request()->routeIs('animals.*') ? 'active' : '' }}">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-drumstick-bite"></i>
+                    <span>Animals</span>
+                </a>
             </li>
 
             <!-- Divider -->
@@ -111,15 +112,6 @@
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
-                    and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
-                    Pro!</a>
             </div>
 
         </ul>
